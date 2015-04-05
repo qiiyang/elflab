381a0de6f05064d6c11256d9ada6606436b4bc5d
Qi I. Yang <yq.yangqi@gmail.com>
2015-04-05 16:57:38 -0700

    1.1.3: Implemented a prototype for He3 measurements

--- devices/T_controllers/T_controller_base.py
+++ devices/T_controllers/T_controller_base.py
@@ -2,6 +2,8 @@
 
 class TControllerBase:
     # Default read function, returns (t, T) of the specified channel
+    def connect(self):     
+        raise Exception("temperature controller not implemented")
     def read(self, ch):     
         raise Exception("temperature controller not implemented")
         
