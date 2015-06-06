729928e5ed63184b75441714599fd9f4f26b8952
Qi I. Yang <yq.yangqi@gmail.com>
2015-06-05 17:26:43 -0700

    1.2.4.1 dev: renamed gui.py to ui.py

--- ui.py
+++ ui.py
@@ -10,6 +10,9 @@ class Prototype(elflab.abstracts.UIBase):
     def __init__(self, gali):
         self.gali = gali
         
+    def quit():
+        pass
+    
     def start(self):
         self.root = tk.Tk()
         self.root.title("Galileo")
@@ -19,28 +22,32 @@ class Prototype(elflab.abstracts.UIBase):
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.rowconfigure(0, weight=1)
         
-        self.buttonPause = ttk.Button(self.mainframe, text="pause")
+        self.buttonPause = ttk.Button(self.mainframe, text="pause", command=self.gali.pause)
         self.buttonPause.grid(column=1, row=1, sticky=tk.W)
         
-        self.buttonResume = ttk.Button(self.mainframe, text="resume")
+        self.buttonResume = ttk.Button(self.mainframe, text="resume", command=self.gali.resume)
         self.buttonResume.grid(column=1, row=2, sticky=tk.W)
         
-        self.buttonStop = ttk.Button(self.mainframe, text="stop")
+        self.buttonStop = ttk.Button(self.mainframe, text="stop", command=self.gali.stop)
         self.buttonStop.grid(column=1, row=3, sticky=tk.W)
         
-        self.buttonQuit = ttk.Button(self.mainframe, text="quit")
-        self.buttonQuit.grid(column=1, row=4, sticky=tk.W)
-        
-        self.buttonPlot = ttk.Button(self.mainframe, text="plot")
+        self.buttonPlot = ttk.Button(self.mainframe, text="plot", command=self.gali.plot)
         self.buttonPlot.grid(column=2, row=1, sticky=tk.W)
         
-        self.buttonAutoOn = ttk.Button(self.mainframe, text="autoscale: on")
+        self.buttonAutoOn = ttk.Button(self.mainframe, text="autoscale: on", command=self.gali.autoscaleOn)
         self.buttonAutoOn.grid(column=2, row=2, sticky=tk.W)
         
-        self.buttonAutoOff = ttk.Button(self.mainframe, text="autoscale: off")
+        self.buttonAutoOff = ttk.Button(self.mainframe, text="autoscale: off", command=self.gali.autoscaleOff)
         self.buttonAutoOff.grid(column=2, row=3, sticky=tk.W)
+        
+        self.buttonClear = ttk.Button(self.mainframe, text="clear plots", command=self.gali.clearPlot)
+        self.buttonClear.grid(column=2, row=4, sticky=tk.W)
+        
+        self.buttonQuit = ttk.Button(self.mainframe, text="quit")
+        self.buttonQuit.grid(column=1, row=5, sticky=tk.W)
         
         for child in self.mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
         
         self.root.mainloop()
+        self.gali.start()
