45397e45af2c26199f35fa1e5e75f895e3719ba8
Qi I. Yang <yq.yangqi@gmail.com>
2015-06-23 12:27:13 -0700

    1.2.6: PPMS analysis: now can handle header lines in the raw file.

--- projects/ppms/analysis.py
+++ projects/ppms/analysis.py
@@ -19,53 +19,53 @@ def import_dc(filename):
     # reading data
     with open(filename, "r", newline='') as f:
         reader = csv.reader(f)
-        next(reader)
         for row in reader:
-            try:
-                ltime.append(float(row[1]))
-            except ValueError:
-                ltime.append(np.nan)
+            if row[0] == "":    # a valid data line only if the comment entry is ""
+                try:
+                    ltime.append(float(row[1]))
+                except ValueError:
+                    ltime.append(np.nan)
+                    
+                try:
+                    lT.append(float(row[3]))
+                except ValueError:
+                    lT.append(np.nan)
+                    
+                try:
+                    lH.append(float(row[4]))
+                except ValueError:
+                    lH.append(np.nan)
+                    
+                try:
+                    lch1R.append(float(row[19]))
+                except ValueError:
+                    lch1R.append(np.nan)
+                    
+                try:
+                    lch2R.append(float(row[20]))
+                except ValueError:
+                    lch2R.append(np.nan)
                 
-            try:
-                lT.append(float(row[3]))
-            except ValueError:
-                lT.append(np.nan)
+                try:
+                    lch3R.append(float(row[21]))
+                except ValueError:
+                    lch3R.append(np.nan)
                 
-            try:
-                lH.append(float(row[4]))
-            except ValueError:
-                lH.append(np.nan)
+                # Calculating the standard errors assuming the stupid default resistivity calculation made by PPMS
+                try:
+                    lch1err.append(float(row[14]) * 1.e3 / float(row[18])**0.5)
+                except ValueError:
+                    lch1err.append(np.nan)
                 
-            try:
-                lch1R.append(float(row[19]))
-            except ValueError:
-                lch1R.append(np.nan)
-                
-            try:
-                lch2R.append(float(row[20]))
-            except ValueError:
-                lch2R.append(np.nan)
-            
-            try:
-                lch3R.append(float(row[21]))
-            except ValueError:
-                lch3R.append(np.nan)
-            
-            # Calculating the standard errors assuming the stupid default resistivity calculation made by PPMS
-            try:
-                lch1err.append(float(row[14]) * 1.e3 / float(row[18])**0.5)
-            except ValueError:
-                lch1err.append(np.nan)
-            
-            try:
-                lch2err.append(float(row[15]) * 1.e3 / float(row[18])**0.5)
-            except ValueError:
-                lch2err.append(np.nan)
-                
-            try:
-                lch3err.append(float(row[16]) * 1.e3 / float(row[18])**0.5)
-            except ValueError:
-                lch3err.append(np.nan)
+                try:
+                    lch2err.append(float(row[15]) * 1.e3 / float(row[18])**0.5)
+                except ValueError:
+                    lch2err.append(np.nan)
+                    
+                try:
+                    lch3err.append(float(row[16]) * 1.e3 / float(row[18])**0.5)
+                except ValueError:
+                    lch3err.append(np.nan)
                 
     # Convert lists to datasets (dicts of 1D numpy arrays)
     ch1 = datasets.DataSet([
