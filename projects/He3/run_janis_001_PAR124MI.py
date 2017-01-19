36391b4e56eea2ef351dfa8e64d5764cce7742a2
Qi I. Yang <yq.yangqi@gmail.com>
2017-01-19 11:53:13 -0800

    Deleting unused old files

--- projects/He3/run_janis_001_PAR124MI.py
+++
@@ -1,6 +0,0 @@
-from elflab import kernels
-import elflab.projects.He3.janis_001_v2 as janis001
-
-if __name__ == '__main__':
-    gui=janis001.Janis001GUI(kernels.Galileo, janis001.Janis001PAR124MI)
-    gui.start()
--- projects/He3/run_janis_s07_PAR124MI.py
+++
@@ -1,6 +0,0 @@
-from elflab import kernels
-import elflab.projects.He3.janis_s07 as s07
-
-if __name__ == '__main__':
-    gui=s07.JanisS07GUI(kernels.Galileo, s07.JanisS07PAR124MI, s07.JanisS07Controller)
-    gui.start()
