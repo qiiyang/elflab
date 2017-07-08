3ec4379d75712eb7524142159079b7fbc89f70f2
Qi I. Yang <yq.yangqi@gmail.com>
2017-07-07 17:12:41 -0700

    v1.3.3.0: mpms, split loadfile() into dc and rso options

--- projects/mpms/analysis.py
+++ projects/mpms/analysis.py
@@ -4,8 +4,8 @@ import numpy as np
 
 import elflab.datasets as datasets
 
-# Import a MPMS data file   
-def loadfile(filename):
+# Import a MPMS DC data file   
+def loadfile_dc(filename):
     # initialize lists for reading
     ltime = []
     lT = []
@@ -16,7 +16,7 @@ def loadfile(filename):
     with open(filename, "r", newline='') as f:
         reader = csv.reader(f)
         for row in reader:
-            if row[0] == "":    # a valid data line only if the comment entry is ""
+            if (len(row) > 0) and (row[0] == ""):    # a valid data line only if the comment entry is ""
                 try:
                     ltime.append(float(row[1]))
                 except ValueError:
@@ -41,7 +41,7 @@ def loadfile(filename):
                     lMerr.append(float(row[5]))
                 except ValueError:
                     lMerr.append(np.nan)
-                
+    
     # Convert lists to datasets (dicts of 1D numpy arrays)
     data = datasets.DataSet([
                             ("time", np.array(ltime, dtype=np.float_, copy=True)),
@@ -49,5 +49,61 @@ def loadfile(filename):
                             ("H", np.array(lH, dtype=np.float_, copy=True)),
                             ("M", np.array(lM, dtype=np.float_, copy=True)),
                             ("err_M", np.array(lMerr, dtype=np.float_, copy=True))
+                            ])
+    return data
+    
+
+# Import a MPMS RSO data file  
+def loadfile_rso(filename):
+    # initialize lists for reading
+    ltime = []
+    lT = []
+    lH = []
+    lM = []
+    lM_noAbs = []
+    lMerr = []
+    # reading data
+    with open(filename, "r", newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if (len(row) > 1) and (row[1] == ""):    # a valid data line only if the comment entry is ""
+                try:
+                    ltime.append(float(row[0]))
+                except ValueError:
+                    ltime.append(np.nan)
+                    
+                try:
+                    lT.append(float(row[3]))
+                except ValueError:
+                    lT.append(np.nan)
+                    
+                try:
+                    lH.append(float(row[2]))
+                except ValueError:
+                    lH.append(np.nan)
+                    
+                try:
+                    lM.append(float(row[4]))
+                except ValueError:
+                    lM.append(np.nan)
+                    
+                try:
+                    lMerr.append(float(row[5]))
+                except ValueError:
+                    lMerr.append(np.nan)
+                    
+                try:
+                    lM_noAbs.append(float(row[18]))
+                except ValueError:
+                    lM_noAbs.append(np.nan)
+    
+    # Convert lists to datasets (dicts of 1D numpy arrays)
+    data = datasets.DataSet([
+                            ("time", np.array(ltime, dtype=np.float_, copy=True)),
+                            ("T", np.array(lT, dtype=np.float_, copy=True)),
+                            ("H", np.array(lH, dtype=np.float_, copy=True)),
+                            ("M", np.array(lM, dtype=np.float_, copy=True)),
+                            ("err_M", np.array(lMerr, dtype=np.float_, copy=True)),
+                            ("M_noAbs", np.array(lM_noAbs, dtype=np.float_, copy=True))
                             ])
     return data
