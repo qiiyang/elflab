8d8fa5daa3be26fae62a3e6a9dd134bd2c849ed4
Qi I. Yang <yq.yangqi@gmail.com>
2017-11-07 17:56:05 -0800

    Implemented AMI model 430 driver

--- devices/magnets/ami.py
+++ devices/magnets/ami.py
@@ -31,5 +31,36 @@ class Model420(MagnetBase):
             
         return (time.perf_counter(), self.H, float("nan"))
         
+        
+
+
+class Model430(MagnetBase):
+    idn_str = "430"   # Identifier string to check
+    def __init__(self, address):
+        self.address = address
+        
+        self.connected = False
+        self.H = float('nan')
+        self.I = float('nan')
+        
+    def connect(self):
+        rm = visa.ResourceManager()
+        self.instrument = rm.open_resource(self.address)
+        idn = self.instrument.query("*idn?")
+        if (self.idn_str not in idn):
+            raise Exception("AMI Model 430 magnet programmer idn string does not match")
+            
+        self.instrument.write("CONFigure:FIELD:UNITS 1")  # Configure the field unit to Tesla
+        print("        AMI Model 430 magnet programmer connected, address={}.".format(self.address))        
+        self.connected = True
+    
+    def read(self): # returns (t, H/Tesla, I_magnet/A)
+        try:        
+            stat = str(self.instrument.query("FIELD:MAGnet?"))
+            self.H = float(stat)
+        except:
+            self.H = float("nan")
+            
+        return (time.perf_counter(), self.H, float("nan"))
 
         
